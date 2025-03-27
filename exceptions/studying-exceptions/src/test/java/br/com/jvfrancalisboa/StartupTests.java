3e19af4058cf2ebcbfcf8a0ffb74a77449557e91
JVFrancaLisboa <logiccoder88@gmail.com>
2025-03-27 15:02:10 -0300

    Created the project to carry out studies on exceptions

---
+++ exceptions/studying-exceptions/src/test/java/br/com/jvfrancalisboa/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.jvfrancalisboa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
